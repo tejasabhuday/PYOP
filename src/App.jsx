35289fed3496761b48073ca87086d6b3eb1538a3
Tejas <tejasabuday@gmail.com>
2024-08-07 00:00:03 +0530

    try

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Benefits from "./components/Benefits";
 import Collaboration from "./components/Collaboration";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Hero from "./components/hero.jsx";
+import Hero from "./components/hero";
 import Pricing from "./components/Pricing";
 import Roadmap from "./components/Roadmap";
 import Services from "./components/Services";
