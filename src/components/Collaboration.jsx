45fda3cc362000b59711bcd817486c07b09afa92
Tejas <tejasabuday@gmail.com>
2024-08-06 23:47:13 +0530

    New changes

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -51,7 +51,6 @@ const Collaboration = () => {
               </li>
             ))}
           </ul>
-
         </div>
 
         <div className="lg:ml-auto xl:w-[38rem] mt-4">
@@ -68,7 +67,7 @@ const Collaboration = () => {
                       src={brainwaveSymbol}
                       width={48}
                       height={48}
-                      alt="brainwave"
+                      alt="PYOP"
                     />
                   </a>
                 </div>
@@ -79,17 +78,13 @@ const Collaboration = () => {
               {collabApps.map((app, index) => (
                 <li
                   key={app.id}
-                  className={`absolute top-0 left-1/2 h-1/2 -ml-[1.6rem] origin-bottom rotate-${
-                    index * 45
-                  }`}
+                  className={`absolute top-0 left-1/2 h-1/2 -ml-[1.6rem] origin-bottom rotate-${index * 45}`}
                 >
                   <a
                     href={app.link}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className={`relative -top-[1.6rem] flex w-[3.2rem] h-[3.2rem] bg-n-7 border border-n-1/15 rounded-xl -rotate-${
-                      index * 45
-                    }`}
+                    className={`relative -top-[1.6rem] flex w-[3.2rem] h-[3.2rem] bg-transparent`}
                   >
                     <RotateItem>
                       <img
